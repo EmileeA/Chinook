dc30bd4f86d5de3057ef4f7d768601b4eecde164
Emilee Mitchell <emcakes@gmail.com>
2020-05-06 12:55:25 -0500

    non usa customers

---
+++ Non_USA_Customers.sql
@@ -0,0 +1,6 @@
+-- non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+select * from Customer
+
+select CustomerId, Country, FirstName, LastName
+from Customer
+where Country != 'USA'
